8443152105b1d3de187bd0fe3c1d0fe8b9251e89
BipraM <bipra.mukharjee@excellisit.net>
2025-07-06 14:27:44 +0530

    add

---
+++ components/CompanionsList.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+type Props = {};
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import Image from "next/image";
+
+interface CompanionsListProps {
+  title: string;
+  companions? : Companion[];
+  classNames? : string;
+}
+
+const CompanionsList = ({title, companions, classNames}: CompanionsListProps) => {
+  return (
+    <article className={cn('companion-list', classNames)}>
+      <h2 className="font-bold text-3xl">Recent Sessions</h2>
+
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="text-lg w-2/3">Lessons</TableHead>
+            <TableHead className="text-lg">Subject</TableHead>
+            <TableHead className="text-lg text-right">Duration</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+
+          {companions?.map(({id, name, subject, topic, duration})=> (
+            <TableRow key={id}>
+              <TableCell>
+                <Link href={`/companions/${id}`}>
+                  <div className="flex items-center gap-2">
+                    <div className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden"
+                    
+                    >
+                      <Image src={`/icons/${subject}.svg`} alt="sub" width={35} height={35}/>
+                    </div>
+                  </div>
+                  {subject}
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+    </article>
+  );
+};
+
+export default CompanionsList;
